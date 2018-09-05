1de4d182eb3cee0fb63f355afa3f34125c889aad
marcus lucious <marcus.j.lucious@outlook.com>
2018-09-05 15:37:27 -0400

    exercise 5A

---
+++ StructsAndEnums/Month.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StructsAndEnums
+{
+    // TODO:
+    enum Month
+    {
+        January, February, March, April,
+        May, June, July, August,
+        September, October, November, December
+    }
+
+}
+    
+    
